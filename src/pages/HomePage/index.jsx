8c25b058ce479134c47076fa303ea70b743c3ea9
Ivana Janoštíková <ivkagre@gmail.com>
2025-05-31 15:31:37 +0200

    useState and buttons worked

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,44 @@
 import './style.css';
+import { useState } from 'react';
 
 export const HomePage = () => {
+
+  const [like, setLike] = useState(0);
+
+  const handleClick = (number)=>{
+    setLike(like + 1);
+
+  }
+
+  const [dislike, setDislike] = useState(0);
+
+  const handleClickDislike = (number) =>{
+    setDislike(dislike + 1);
+  }
   return (
+    <>
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button id="btn-up" className="btn-like btn-like--up" onClick={handleClick}></button>
+          <span id="likes-up" className="likes-count likes-count--up" onClick={handleClickDislike}>0</span>
+          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <span id="likes-down" className="likes-count likes-count--down">0</span>
+        </div>
+      </div>
+      </div>
+   </>
   );
 };
