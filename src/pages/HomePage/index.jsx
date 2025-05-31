7c391ae4339d9cedd2c0c8705f82e2911115880a
Ivana Janoštíková <ivkagre@gmail.com>
2025-05-31 18:54:43 +0200

    Exercise 16 finished

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,44 +1,25 @@
+import { Jokes } from '../../components/Jokes/Jokes';
 import './style.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const HomePage = () => {
+  const [jokes, setJokes] = useState([]);
 
-  const [like, setLike] = useState(0);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('http://localhost:4000/api/jokes');
+      const jsonData = await response.json();
+      setJokes(jsonData.data);
+    };
+    getData();
+  }, []);
 
-  const handleClick = ()=>{
-    setLike(like + 1);
-
-  }
-
-  const [dislike, setDislike] = useState(0);
-
-  const handleClickDislike = () =>{
-    setDislike(dislike + 1);
-  }
   return (
     <>
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up" onClick={handleClick}></button>
-          <span id="likes-up" className="likes-count likes-count--up">{like}</span>
-          <button id="btn-down" className="btn-like btn-like--down" onClick={handleClickDislike}></button>
-          <span id="likes-down" className="likes-count likes-count--down">{dislike}</span>
-        </div>
-      </div>
-      </div>
-   </>
+      {jokes.map(joke => <Jokes key={joke.id} userAvatar={joke.avatar} userName={joke.name} text={joke.text} likes={joke.likes} dislikes ={joke.dislikes}  />
+        
+      )}
+      
+    </>
   );
 };
